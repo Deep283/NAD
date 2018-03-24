c5e375e8a37306a7d6695718c0d68db07a22f413
Deep283 <deepmwn@gmail.com>
2018-03-24 17:11:39 +0530

    Update EliminationMatlab.m

--- EliminationMatlab.m
+++ EliminationMatlab.m
@@ -1,20 +1,23 @@
+tic
+n = 3;
 a = [1 1 1 6 ; 3 4 5 26 ; 1 2 1 8];
-for i = 1 : n
-    for j = i+1 : n
+parfor i = 1 : n
+    parfor j = i+1 : n
  	    k = a(j,i)/a(i,i);
-	    for z = i : n+1
+	    parfor z = i : n+1
 	 	    a(j,z) = a(j,z) - ( k * a(i,z) );
 	    end
     end
 end
 x = [ 0 0 0 ];
 x(n) = a(n,n+1)/a(n,n);
-for i = n-1 : -1 : 1
+parfor i = n-1 : -1 : 1
     sum = 0;
-    for j = i+1 : n
+    parfor j = i+1 : n
        sum = sum + a(i,j) * x(j);
     end
     x(i) = (a(i,n+1) - sum ) / a(i,i);
 end
 disp(a)
 disp(x)
+time = toc
